9492b70e343ac6b0bc073493c2a20b9305d12272
Boban Sugareski <bobansugareski@Boban-Sugareski-MacBook-Pro.local>
2020-02-08 19:28:12 +0100

    Add open-sans and open-sans-bold fonts

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true)
+        }}
+      />
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is the SuccessfulMe app!</Text>
+      <Text style={styles.message}>This is the SuccessfulMe app!</Text>
     </View>
   );
 }
@@ -14,6 +36,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  message: {
+    fontFamily: 'open-sans-bold'
+  }
 });
